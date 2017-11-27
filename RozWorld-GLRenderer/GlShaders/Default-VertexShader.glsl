ca5413f4f256f47f57af484bff44f7cc54d406ea
Rory Fewell <roryfewell@hotmail.com>
2017-11-27 23:05:56 +0000

    Prelim: Add inital shader files to project

---
+++ RozWorld-GLRenderer/GlShaders/Default-VertexShader.glsl
@@ -0,0 +1,19 @@
+﻿#version 330 core
+
+layout(location = 0) in vec2 VertexPosition;
+layout(location = 1) in vec2 VertexUV;
+
+out vec2 UV;
+
+uniform vec2 CanvasResolution;
+uniform vec2 UvMapResolution;
+
+
+void main()
+{
+    gl_Position.xy = vec2(VertexPosition.x / CanvasResolution.x, VertexPosition.y / CanvasResolution.y);
+    gl_Position.z = 1.0;
+    gl_Position.w = 1.0;
+    
+    UV = vec2(VertexUV.x / UvMapResolution.x, VertexUV.y / UvMapResolution.y);
+}
---
+++ RozWorld-GLRenderer/GlShaders/FontCache-FragmentShader.glsl
@@ -0,0 +1,15 @@
+﻿#version 330 core
+
+in vec2 UV;
+
+layout(location = 0) out vec3 Colour;
+
+uniform float fTime;
+uniform sampler2D TextureSampler;
+
+
+void main()
+{
+    //Colour = vec3(abs(sin(fTime)), abs(cos(fTime)), abs(sin(fTime / 5f)));
+    Colour = texture(TextureSampler, UV).rgb;
+}
